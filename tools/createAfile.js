034e627ac317ce9aa0bb8c31f94932a34fc12ff1
Karthikreddy0165 <Karthikredddy0165@gmail.com>
2024-09-17 14:56:28 +0530

    made neccesary changes

--- tools/createAfile.js
+++ tools/createAfile.js
@@ -8,14 +8,9 @@ const __dirname = path.dirname(__filename);
 const createAfile = ( req, res) => {
     const {fileName,fileData} = req.body
     const filepath = path.join(__dirname,'../root',fileName)
-    if(!es(filepath)){
         WFS(filepath, fileData)
         res.status(200).json({
             "message": "File created successfully"
         })
-    }
-    res.status(400).json({
-        "message": "File already exists"
-    })
 };
 export default createAfile;
--- tools/deleteAFile.js
+++ tools/deleteAFile.js
@@ -13,7 +13,7 @@ const deleteAFile = (req, res) => {
     if(es(filepath)){
         uls(filepath)
         res.status(200).json({
-            "message": "File Deleted Successfully"
+            "message": "File deleted successfully"
         })
     }
     else{
